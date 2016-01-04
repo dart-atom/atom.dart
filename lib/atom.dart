b2448065cc4e9dc48e835b0ad25405a838f98837
Devon Carew <devoncarew@gmail.com>
2016-01-04 13:51:05 -0800

    change to a dart:js style package init

--- lib/atom.dart
+++ lib/atom.dart
@@ -20,18 +20,58 @@ final Logger _logger = new Logger('atom');
 /// The singleton instance of [Atom].
 final Atom atom = new Atom();
 
-void registerPackage(AtomPackage package) {
-  Map packageInfo = {
-    'activate': package.activate,
-    'deactivate': package.deactivate,
-    'config': package.config(),
-    'serialize': package.serialize
-  };
+AtomPackage _package;
 
-  context[package.id] = jsify(packageInfo);
+/// Call this method once from the main method of your package.
+///
+///     main() => registerPackage(new MyFooPackage());
+void registerPackage(AtomPackage package) {
+  if (_package != null) {
+    throw new StateError('can only register one package');
+  }
+
+  _package = package;
+
+  final JsObject exports = context['module']['exports'];
+
+  exports['activate'] = _package.activate;
+  exports['deactivate'] = _package.deactivate;
+  exports['config'] = jsify(_package.config());
+  exports['serialize'] = _package.serialize;
+
+  package._registeredMethods.forEach((methodName, f) {
+    exports[methodName] = (arg) {
+      var result = f(arg);
+      if (result is Disposable) {
+        // Convert the returned Disposable to a JS object.
+        Map m = {'dispose': result.dispose};
+        return jsify(m);
+      } else if (result is List || result is Map) {
+        return jsify(result);
+      } else if (result is JsObject) {
+        return result;
+      } else {
+        return null;
+      }
+    };
+  });
+  package._registeredMethods = null;
 }
 
+// void registerPackage(AtomPackage package) {
+//   Map packageInfo = {
+//     'activate': package.activate,
+//     'deactivate': package.deactivate,
+//     'config': package.config(),
+//     'serialize': package.serialize
+//   };
+//
+//   context[package.id] = jsify(packageInfo);
+// }
+
 abstract class AtomPackage {
+  Map<String, Function> _registeredMethods = {};
+
   final String id;
 
   AtomPackage(this.id);
@@ -50,6 +90,23 @@ abstract class AtomPackage {
   Future<String> getPackageVersion() {
     return loadPackageJson().then((Map map) => map['version']) as Future<String>;
   }
+
+  // /// Register a method for a service callback (`consumedServices`).
+  // void registerServiceConsumer(String methodName, Disposable callback(JsObject obj)) {
+  //   if (_registeredMethods == null) {
+  //     throw new StateError('method must be registered in the package ctor');
+  //   }
+  //   _registeredMethods[methodName] = callback;
+  //   return null;
+  // }
+  //
+  // void registerServiceProvider(String methodName, JsObject callback()) {
+  //   if (_registeredMethods == null) {
+  //     throw new StateError('method must be registered in the package ctor');
+  //   }
+  //   _registeredMethods[methodName] = callback;
+  //   return null;
+  // }
 }
 
 class Atom extends ProxyHolder {
