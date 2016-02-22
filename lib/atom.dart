ad6ad348e50649d8326884fa087a414d39b8fa13
danrubel <danrubel@google.com>
2016-02-21 22:54:51 -0500

    fix the build

--- lib/atom.dart
+++ lib/atom.dart
@@ -610,7 +610,7 @@ class Panel extends ProxyHolder {
 
   Stream<bool> get onDidChangeVisible => eventStream('onDidChangeVisible') as Stream<bool>;
   Stream<Panel> get onDidDestroy =>
-      eventStream('onDidDestroy').map((obj) => new Panel(obj)) as Stream<Panel>;
+      eventStream('onDidDestroy').map((obj) => new Panel(obj));
 
   bool isVisible() => invoke('isVisible');
   void show() => invoke('show');
